74f813802420819323cd62919f7cb2ed7b7730a5
blueqc <arth.meunier@gmail.com>
2023-09-27 10:17:04 -0400

    commit 2

--- FizzBuzz/Program.cs
+++ FizzBuzz/Program.cs
@@ -3,11 +3,14 @@
     public class Program
     {
         private const int MIN = 1;
-        private const int MAX = 100;
+        
 
         public static void Main(string[] args)
         {
-            for (int i = MIN; i <= MAX; i++)
+            int max = AskMaxNumber();
+
+
+            for (int i = MIN; i <= max; i++)
             {
                 bool isDivisbleBy3 = IsDivisibleBy(i, 3);
                 bool isDivisbleBy5 = IsDivisibleBy(i, 5);
@@ -34,6 +37,14 @@
         private static bool IsDivisibleBy(int value, int divisor)
         {
             return value % divisor == 0;
+        }
+
+        public static int AskMaxNumber()
+        {
+            Console.WriteLine("Entrez un nombre :");
+            int max = int.Parse(Console.ReadLine());
+
+            return max;
         }
     }
 }
